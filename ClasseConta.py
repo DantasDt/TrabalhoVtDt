7dcd92592d3c671b24fc77c6a5b823cc178c2d44
vtotrem <vitorquadrodemodelar@gmail.com>
2026-05-20 16:46:08 -0300

    Update ClasseConta.py

--- ClasseConta.py
+++ ClasseConta.py
@@ -1,19 +1,20 @@
 class Conta:
-    def __init__(self, nome, saldo):
+    def _init_(self, nome, saldo):
         self.nome = nome
         self.saldo = saldo
     def saca(self, valor):
        if 0 < valor <= self.saldo:
            self.saldo -= valor
            print("saque de R$",valor, "foi efetuado com sucesso!")
+           print("Saldo atual: ", self.saldo)
        else: 
            print("Saldo insuficiente, pobre!")
     def deposita(self, valor):
         if 0 < valor:
             self.saldo += valor
             print("Depósito de R$",valor, "efetuado com sucesso!")
+            print("Saldo atual: ", self.saldo)
         else:
             print("Não é possível depositar esse valor!")
     def calcularend(self):
-        return self.saldo * 0.1
-
+        print("Rendimento mensal: ", self.saldo * 0.1)
